df04a5c45ad5ab7bcb3f58f774d24ccf13c2c6ac
beatriceiaria01-dotcom <beatrice.iaria01@gmail.com>
2026-03-22 19:35:10 +0900

    Update tentativeonline.js

--- tentativeonline.js
+++ tentativeonline.js
@@ -169,7 +169,7 @@ function routineBegin(thisTrial, blockName) {
                 mainImage.setSize([1.10, 0.45]); 
             } else if (blockName === 'MX') {
                 // Aumentata dimensione generale a 0.55
-                mainImage.setPos([0, 0.08]); 
+                mainImage.setPos([0, 0.13]); 
                 mainImage.setSize([0.65, 0.45]); 
             } else {
                 mainImage.setPos([0, 0.10]);
